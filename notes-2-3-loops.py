fb27df32ab94253083f028ac95e9dac07890aef5
Tim Ubial <tubial@sd38.bc.ca>
2024-04-05 10:47:21 -0700

    filled out some of the loops notes

--- notes-2-3-loops.py
+++ notes-2-3-loops.py
@@ -1,3 +1,44 @@
 # Loops and Iteration
 # Author: Ubial
 # 5 April 2024
+
+# Print "something" 10 times
+for _ in range(10):
+    print("something")
+
+# Print out every item in my grocery list
+grocery_list = [
+    "dishwasher tabs",
+    "aluminium foil",
+    "blueberry muffins",
+    "RTX 4070 Super",
+]
+
+# Stop if we reach blueberry muffins
+for item in grocery_list:
+    print("----")
+    print(f"* {item}")
+
+    if item == "blueberry muffins":
+        break
+
+# Count from 0 to 9
+for i in range(1000):
+    print(i)
+
+    # Modulo
+    if i % 2 == 0:
+        print(f"{i} is an even number")
+
+# Rewrite the above for loop as a while loop
+counter = 0
+
+while counter < 1000:
+    if counter % 2 == 0:
+        print(f"{counter} is an even number")
+    else:
+        print(counter)
+
+    # increment counter by 1
+    # counter = counter + 1
+    counter += 1
