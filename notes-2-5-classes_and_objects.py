1aeab4be6786ccd626f299564df35e7bba7c3815
Tim Ubial <tubial@sd38.bc.ca>
2024-04-15 11:08:04 -0700

    continued with classes notes

--- notes-2-5-classes_and_objects.py
+++ notes-2-5-classes_and_objects.py
@@ -2,3 +2,58 @@
 
 # Big Ideas:
 #   - Classes allow us to couple data and functions together
+
+
+# Create a class that represents a Pokemon
+class Pokemon:  # always name classes with capital
+    def __init__(self):
+        """Constructor: contains all properties
+        of a Pokemon. It also contains the default
+        state of the properties.
+        """
+        self.name = ""
+        self.id = 0
+        self.weight = 0
+        self.height = 0
+        self.type = "Normal"
+
+
+def main():
+    # Create two Pokemon
+
+    # Create something 'Pikachu'-like
+    pokemon_one = Pokemon()
+
+    # Access the properties inside pokemon_one
+    # Print the name of pokemon_one
+    # print(pokemon_one.name)
+    # print(pokemon_one.type)
+
+    # Change the values of the propeties
+    pokemon_one.name = "Pikachu"
+    pokemon_one.type = "Electric"
+    pokemon_one.id = 25
+
+    # Print the values from pokemon_one
+    # print(pokemon_one.name)
+    # print(pokemon_one.type)
+
+    # TODO: Create something 'Squirtle'-like
+    #  - Create a new Pokemon object
+    #      - Store this in variable pokemon_two
+    #  - Squirtle's Pokedex id is 4
+    #  - Squirtle's type is Water
+
+    pokemon_two = Pokemon()
+    pokemon_two.name = "Squirtle"
+    pokemon_two.type = "Water"
+    pokemon_two.id = 4
+
+    # To test, print out all of squirtle's properties
+    print(pokemon_two.name)
+    print(pokemon_two.id)
+    print(pokemon_two.type)
+
+
+if __name__ == "__main__":
+    main()
