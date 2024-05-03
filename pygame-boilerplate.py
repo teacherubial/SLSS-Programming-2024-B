dc8db8410de222968901cfdee37e878f61c858e9
Tim Ubial <tubial@sd38.bc.ca>
2024-05-03 11:51:49 -0700

    Pygame boilerplate

---
+++ pygame-boilerplate.py
@@ -0,0 +1,57 @@
+import pygame as pg
+
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+
+WIDTH = 1280  # Pixels
+HEIGHT = 720
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+
+def start():
+    """Environment Setup and Game Loop"""
+
+    pg.init()
+
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+
+    # All sprites go in this sprite Group
+    all_sprites = pg.sprite.Group()
+
+    pg.display.set_caption("<WINDOW TITLE HERE>")
+
+    # --Main Loop--
+    while not done:
+        # --- Event Listener
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+
+        # --- Update the world state
+
+        # --- Draw items
+        screen.fill(BLACK)
+
+        # Update the screen with anything new
+        pg.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
